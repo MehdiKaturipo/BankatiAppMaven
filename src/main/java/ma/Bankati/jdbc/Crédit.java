b5fe48e8c575c19d0933575d012f05bac8a1c547
Soussan Mehdi <118851400+MehdiKaturipo@users.noreply.github.com>
2023-03-18 16:37:37 +0100

    API JAVA :DAO + FACTORY+JDBC

---
+++ src/main/java/ma/Bankati/jdbc/Crédit.java
@@ -0,0 +1,33 @@
+package ma.Bankati.jdbc;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @AllArgsConstructor @NoArgsConstructor
+
+public class Crédit {
+    private Long id;
+    private Double capitale_Emprunté;
+    private Integer nombreDeMois;
+    private Double taux_Mensuel;
+    private String Demandeur;
+    private Double mensualité;
+
+    @Override
+    public String toString() {
+        var creditstr ="= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =\n";
+        creditstr += "=> Crédit N°                                         :" + getId() +"\n";
+        creditstr+= "=>  nom du demandeur de credit                        : " + getDemandeur() +"  \n ";
+        creditstr += "---------------------------------------------------------------------------------\n";
+        creditstr += "=> Capitale emprunté                                 : " + getCapitale_Emprunté() +" \n";
+        creditstr += "=> Nombre de mois                                    : " + getNombreDeMois() +"\n";
+        creditstr += "=> Taux mensuel                                      : " + getTaux_Mensuel() +"\n";
+        creditstr += "--------------------------------------------------------------------------------\n";
+        creditstr += "=> Mensualité                                        : "
+                               + (getMensualité() == 0.0 ? "NON-CALCULE": getMensualité()+"DH/mois") +"\n";
+        creditstr +="= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =\n";
+        return  creditstr;
+
+    }
+}
---
+++ src/main/java/ma/Bankati/jdbc/TestJBDC.java
@@ -0,0 +1,72 @@
+package ma.Bankati.jdbc;
+
+import lombok.*;
+
+import java.sql.*;
+
+public class TestJBDC {
+
+
+
+    public static void main(String[] args) {
+        var url ="jdbc:mysql://localhost:3306/bankati" ;
+        var login ="root";
+        var pass ="";
+        var driver ="com.mysql.cj.jdbc.Driver";
+        Connection connection = null ;
+         Statement statement = null;
+        PreparedStatement sc = null;
+        ResultSet resultSet = null;
+        ResultSetMetaData resultMetaDta = null;
+
+        try {
+            Class.forName(driver);
+            System.out.println("Driver loaded");
+            connection = DriverManager.getConnection(url, login, pass);
+            System.out.println("Database connected");
+
+            //Statement
+            sc=connection.prepareStatement("select * from Crédit ");
+            resultSet = sc.executeQuery();
+            resultMetaDta = resultSet.getMetaData();
+            System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------");
+            while (resultSet.next()){
+                for (int i =1;i<=resultMetaDta.getColumnCount();i++)
+
+                    System.out.print("\t"+resultMetaDta.getColumnName(i).toUpperCase()+
+                            " : "+resultSet.getObject(i).toString() + "\t |");
+                System.out.println();
+                System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------");
+
+
+//                var id = resultSet.getLong("id");
+//                var cp = resultSet.getDouble("capital");
+//                var nbrMois = resultSet.getInt("nbr_mois");
+//                var taux = resultSet.getDouble("taux_mensialité");
+//                var demandeur = resultSet.getString("demandeur");
+//                var mensualité = resultSet.getDouble("mensualité");
+//
+//                Crédit credit = new Crédit(id,cp,nbrMois,taux,demandeur,mensualité);
+//                System.out.println(credit);
+
+            }
+
+
+
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("Driver not found");
+        } catch (SQLException e) {
+            System.out.println("Database not connected");
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    System.out.println("Database not closed");
+                }
+            }
+
+        }
+    }
+}
